5814fa2dfbcab8bf7fe2e1a7d1729976fb44fbd8
LoaiSaberC4r <loai.sabir@outlook.com>
2025-10-19 19:14:44 +0300

    Start to Add Logining Fields

---
+++ BuildingBlock.Api/Logging/DomainAreaEnricher.cs
@@ -0,0 +1,12 @@
+﻿namespace BuildingBlock.Api.Logging
+{
+    public sealed class DomainAreaEnricher : ILogEventEnricher
+    {
+        private readonly string _area;
+
+        public DomainAreaEnricher(string area) => _area = area;
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory factory)
+            => logEvent.AddPropertyIfAbsent(factory.CreateProperty("domainArea", _area));
+    }
+}
