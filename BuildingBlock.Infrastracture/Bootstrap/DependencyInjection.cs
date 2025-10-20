c4565b7d8eaef70b7551b9b7c13bca11e379fc3f
LoaiSaberC4r <loai.sabir@outlook.com>
2025-10-20 14:19:53 +0300

    Adding Audiability and SoftDelted Global Interceptor

--- BuildingBlock.Infrastracture/Bootstrap/DependencyInjection.cs
+++ BuildingBlock.Infrastracture/Bootstrap/DependencyInjection.cs
@@ -1,7 +1,11 @@
 ﻿using BuildingBlock.Application.Repositories;
+using BuildingBlock.Application.Time;
+using BuildingBlock.Infrastracture.Interceptors;
 using BuildingBlock.Infrastracture.Repositories;
+using BuildingBlock.Infrastracture.Time;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace BuildingBlock.Infrastracture.Bootstrap
 {
@@ -19,7 +23,39 @@ namespace BuildingBlock.Infrastracture.Bootstrap
 
             return services;
         }
+
+        public static IServiceCollection AddBuildingBlockAuditing(this IServiceCollection services)
+        {
+            services.TryAddSingleton<IDateTimeProvider, SystemDateTimeProvider>();
+            services.AddScoped<AuditableEntitiesInterceptor>();
+            return services;
+        }
+
+        public static IServiceCollection AddBuildingBlockSoftDelete(this IServiceCollection services)
+        {
+            services.TryAddSingleton<IDateTimeProvider, SystemDateTimeProvider>(); // لن يُضاف مرة ثانية
+            services.AddScoped<SoftDeleteEntitiesInterceptor>();
+            return services;
+        }
+
+        public static IServiceCollection AddBuildingBlockAuditingAndSoftDelete(this IServiceCollection services)
+        {
+            return services
+                .AddBuildingBlockAuditing()
+                .AddBuildingBlockSoftDelete();
+        }
     }
 }
 
 //builder.Services.AddEfInfrastructure<ApplicationDbContext>();
+//builder.Services.AddBuildingBlockAuditing();
+/*
+ * builder.Services.AddDbContext<ApplicationDbContext>((sp, options) =>
+{
+    var interceptor = sp.GetRequiredService<AuditableEntitiesInterceptor>();
+
+    options
+        .UseSqlServer(builder.Configuration.GetConnectionString("Default"))
+        .AddInterceptors(interceptor);
+});
+ */
